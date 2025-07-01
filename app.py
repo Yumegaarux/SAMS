e84ce207577744adb7259ff039647c8b1a35a583
Yumegaarux <zaldarriagajanvictor@gmail.com>
2025-07-01 10:52:48 +0800

    added css components, along with inheritance from index.html

--- app.py
+++ app.py
@@ -11,5 +11,21 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route("/control")
+def control():
+    return render_template('control.html')
+
+@app.route("/history")
+def history():
+    return render_template('history.html')
+
+@app.route("/help")
+def help():
+    return render_template('help.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
